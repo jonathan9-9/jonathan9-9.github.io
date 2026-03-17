5cef696c6e120db1f8869bb2b71d7061399d9c67
Jonathan <jonathancornejox@gmail.com>
2026-03-16 17:31:40 -0700

    vite config added base url

--- personal-portfolio/vite.config.js
+++ personal-portfolio/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "https://jonathan9-9.github.io/",
   plugins: [react()],
-})
+});
