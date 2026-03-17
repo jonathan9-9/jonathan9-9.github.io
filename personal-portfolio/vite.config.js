8a9a47d73ddcfe75750b309962087c7a6de67d0e
Jonathan <jonathancornejox@gmail.com>
2026-03-16 17:57:05 -0700

    Updates

--- personal-portfolio/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: "https://jonathan9-9.github.io/",
-  plugins: [react()],
-});
